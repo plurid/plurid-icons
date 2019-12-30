657e48afea9a76aac430949844e694b27db81c73
Jan Cavel <caveljan@gmail.com>
2019-12-30 13:43:40 +0200

    plurid-icons-react > dev: general, plurid-specifics icons

--- packages/plurid-icons-react/source/icons/index.ts
+++ packages/plurid-icons-react/source/icons/index.ts
@@ -1,9 +1,27 @@
-import PluridIconAdd from './PluridIconAdd';
-import PluridIconMore from './PluridIconMore';
+/** general */
+import PluridIconAdd from './general/PluridIconAdd';
+import PluridIconMore from './general/PluridIconMore';
+
+/** plurid specific */
+import PluridIconDocuments from './plurid-specific/PluridIconDocuments';
+import PluridIconFirstPerson from './plurid-specific/PluridIconFirstPerson';
+import PluridIconGlobal from './plurid-specific/PluridIconGlobal';
+import PluridIconRotate from './plurid-specific/PluridIconRotate';
+import PluridIconScale from './plurid-specific/PluridIconScale';
+import PluridIconTranslate from './plurid-specific/PluridIconTranslate';
 
 
 
 export {
+    /** general */
     PluridIconAdd,
     PluridIconMore,
+
+    /** plurid specific */
+    PluridIconDocuments,
+    PluridIconFirstPerson,
+    PluridIconGlobal,
+    PluridIconRotate,
+    PluridIconScale,
+    PluridIconTranslate,
 }
