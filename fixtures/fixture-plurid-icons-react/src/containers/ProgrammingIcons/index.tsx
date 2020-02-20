4155fb6984a563a90aaa141a9871a178b5427af1
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-20 12:33:34 +0200

    plurid-icons-react > fixture: App

---
+++ fixtures/fixture-plurid-icons-react/src/containers/ProgrammingIcons/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import {
+    Theme,
+} from '@plurid/plurid-themes';
+
+import {
+    PluridIconAccessor,
+} from '@plurid/plurid-icons-react';
+
+import IconSet from '../../components/IconSet';
+
+import {
+    IconListItem,
+} from '../../interfaces';
+
+
+
+const icons: IconListItem[] = [
+    {
+        Element: PluridIconAccessor,
+        title: 'accessor',
+    },
+];
+
+
+interface PluridSpecificIconsProperties {
+    theme: Theme;
+    iconSize: 'small' | 'normal' | 'large';
+    titleAppearTime: number;
+    titleDisappearTime: number;
+}
+
+const PluridSpecificIcons: React.FC<PluridSpecificIconsProperties> = (properties) => {
+    const {
+        theme,
+        iconSize,
+        titleAppearTime,
+        titleDisappearTime,
+    } = properties;
+
+    return (
+        <IconSet
+            title="plurid' specific icons"
+        >
+            {icons.map(icon => {
+                const {
+                    Element,
+                    title,
+                } = icon;
+
+                return (
+                    <Element
+                        key={title}
+                        theme={theme}
+                        title={title}
+                        size={iconSize}
+                        titleAppearTime={titleAppearTime}
+                        titleDisappearTime={titleDisappearTime}
+                    />
+                );
+            })}
+        </IconSet>
+    );
+}
+
+
+export default PluridSpecificIcons;
