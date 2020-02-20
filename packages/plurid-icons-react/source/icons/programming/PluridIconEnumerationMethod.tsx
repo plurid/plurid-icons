ff8523399a1ccc088511060f548cb90425d3bb2f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-20 17:11:31 +0200

    plurid-icons-react > dev: icons > programming

--- packages/plurid-icons-react/source/icons/programming/PluridIconEnumerationMethod.tsx
+++ packages/plurid-icons-react/source/icons/programming/PluridIconEnumerationMethod.tsx
@@ -8,7 +8,7 @@ import {
 
 
 
-const PluridIconEnumerationMember: React.FC<PluridIconProperties> = (
+const PluridIconEnumerationMethod: React.FC<PluridIconProperties> = (
     properties,
 ) => {
     return (
@@ -16,10 +16,12 @@ const PluridIconEnumerationMember: React.FC<PluridIconProperties> = (
             {...properties}
         >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
+                <polygon points="538.71 411.61 279.46 653.8 54.07 418.67 538.71 411.61"/>
+                <polygon points="945.93 281.1 477.28 718.9 405.38 643.88 707.96 361.22 142.31 369.46 69.85 293.86 945.93 281.1"/>
             </svg>
         </PluridIcon>
     );
 }
 
 
-export default PluridIconEnumerationMember;
+export default PluridIconEnumerationMethod;
