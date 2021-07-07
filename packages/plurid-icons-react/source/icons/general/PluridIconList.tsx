f21d42671c3b42bfb795ed60fc5de4fab9bd2352
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-07-07 11:48:20 +0300

    plurid-icons-react > dev: icons

--- packages/plurid-icons-react/source/icons/general/PluridIconList.tsx
+++ packages/plurid-icons-react/source/icons/general/PluridIconList.tsx
@@ -14,12 +14,12 @@ const PluridIconList: React.FC<PluridIconProperties> = (properties) => {
             {...properties}
         >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
-                <rect class="cls-1" x="251" y="451.32" width="700" height="97.35"/>
-                <rect class="cls-1" x="54" y="451.32" width="105" height="97.35"/>
-                <rect class="cls-1" x="251" y="167.32" width="700" height="97.35"/>
-                <rect class="cls-1" x="54" y="167.32" width="105" height="97.35"/>
-                <rect class="cls-1" x="251" y="735.32" width="700" height="97.35"/>
-                <rect class="cls-1" x="54" y="735.32" width="105" height="97.35"/>
+                <rect x="251" y="451.32" width="700" height="97.35"/>
+                <rect x="54" y="451.32" width="105" height="97.35"/>
+                <rect x="251" y="167.32" width="700" height="97.35"/>
+                <rect x="54" y="167.32" width="105" height="97.35"/>
+                <rect x="251" y="735.32" width="700" height="97.35"/>
+                <rect x="54" y="735.32" width="105" height="97.35"/>
             </svg>
         </PluridIcon>
     );
